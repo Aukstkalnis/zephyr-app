0e7e0a6f9d82ddeff97a1ea3f487261ff1b54c76
Deimantas Aukštkalnis <deimantas.aukstkalnis@teltonika.lt>
2020-03-17 16:46:40 +0200

    Trying to fix Kconfig.

--- external_bluenrg_soc_drivers/zephyr/serial/uart_bluenrg.c
+++ external_bluenrg_soc_drivers/zephyr/serial/uart_bluenrg.c
@@ -21,7 +21,7 @@
 #include <drivers/clock_control.h>
 
 #include <linker/sections.h>
-#include <clock_bluenrg.h>
+#include <drivers/clock_control/clock_bluenrg.h>
 #include "uart_bluenrg.h"
 
 #include <logging/log.h>
@@ -35,26 +35,21 @@ LOG_MODULE_REGISTER(uart_bluenrg);
 #define UART_STRUCT(dev)					\
 	((UART_Type *)(DEV_CFG(dev))->uconf.base)
 
-#define TIMEOUT            1000
-#define UART_CLOCK          (16000000)
+#define TIMEOUT 1000
+#define UART_CLOCK       (16000000)
 #define UART_CLOCK_CYCLE16        (16)      
 #define UART_CLOCK_CYCLE8         (8)
 
 static inline void uart_bluenrg_set_baudrate(struct device *dev, u32_t baud_rate)
 {
-	// const struct uart_bluenrg_config *config = DEV_CFG(dev);
-	// struct uart_bluenrg_data *data = DEV_DATA(dev);
-	UART_InitType *UartInstance = UART_STRUCT(dev);
-	UartInstance->UART_BaudRate = baud_rate;
-
 	// u32_t clock_rate;
 	uint32_t divider;
   	uint16_t ibrd, fbrd;
 
 	if (UART->CR_b.OVSFACT == 0) {
-		divider = (UART_CLOCK<<7) / (UART_CLOCK_CYCLE16 * UartInstance->UART_BaudRate);
+		divider = (UART_CLOCK<<7) / (UART_CLOCK_CYCLE16 * baud_rate);
 	} else {
-		divider = (UART_CLOCK<<7) / (UART_CLOCK_CYCLE8 * UartInstance->UART_BaudRate);
+		divider = (UART_CLOCK<<7) / (UART_CLOCK_CYCLE8 * baud_rate);
 	}
 	ibrd = divider >> 7;
 	UART->IBRD = ibrd;
@@ -67,10 +62,7 @@ static inline void uart_bluenrg_set_baudrate(struct device *dev, u32_t baud_rate
 }
 
 static inline void uart_bluenrg_set_parity(struct device *dev, u32_t parity)
-{
-	UART_InitType *UartInstance = UART_STRUCT(dev);
-	UartInstance->UART_Parity = parity;
-	
+{	
 	switch(parity) {
 		case UART_Parity_No: {
 			UART->LCRH_TX_b.PEN_TX = RESET;
@@ -284,26 +276,26 @@ static int uart_bluenrg_configure(struct device *dev,
 	// }
 	UART_Cmd(DISABLE);
 
-	// if (parity != uart_bluenrg_get_parity(dev)) {
-	// 	uart_bluenrg_set_parity(dev, parity);
-	// }
+	if (parity != uart_bluenrg_get_parity(dev)) {
+		uart_bluenrg_set_parity(dev, parity);
+	}
 
-	// if (stopbits != uart_bluenrg_get_stopbits(dev)) {
-	// 	uart_bluenrg_set_stopbits(dev, stopbits);
-	// }
+	if (stopbits != uart_bluenrg_get_stopbits(dev)) {
+		uart_bluenrg_set_stopbits(dev, stopbits);
+	}
 
-	// if (databits != uart_bluenrg_get_databits(dev)) {
-	// 	uart_bluenrg_set_databits(dev, databits);
-	// }
+	if (databits != uart_bluenrg_get_databits(dev)) {
+		uart_bluenrg_set_databits(dev, databits);
+	}
 
-	// if (flowctrl != uart_bluenrg_get_hwctrl(dev)) {
-	// 	uart_bluenrg_set_hwctrl(dev, flowctrl);
-	// }
+	if (flowctrl != uart_bluenrg_get_hwctrl(dev)) {
+		uart_bluenrg_set_hwctrl(dev, flowctrl);
+	}
 
-	// if (cfg->baudrate != data->baud_rate) {
-	// 	uart_bluenrg_set_baudrate(dev, cfg->baudrate);
-	// 	data->baud_rate = cfg->baudrate;
-	// }
+	if (cfg->baudrate != data->baud_rate) {
+		uart_bluenrg_set_baudrate(dev, cfg->baudrate);
+		data->baud_rate = cfg->baudrate;
+	}
 
 	UART_Cmd(ENABLE);
 	return 0;
@@ -326,23 +318,16 @@ static int uart_bluenrg_config_get(struct device *dev, struct uart_config *cfg)
 
 static int uart_bluenrg_poll_in(struct device *dev, unsigned char *c)
 {
-	// UART_InitType *UartInstance = UART_STRUCT(dev);
-
+	UART_Type *uart = UART_STRUCT(dev);
 	/* Clear overrun error flag */
 	if (UART_GetFlagStatus(UART_FLAG_OE)) {
 		UART_ClearFlag(UART_FLAG_OE);
 	}
-	while (UART_GetFlagStatus(UART_FLAG_RXFE) == SET) {
+	while (UART_GetFlagStatus(UART_FLAG_RXFE) == RESET) {
       ;
     }
 
-	CKGEN_SOC_Type* clock = CKGEN_SOC;
-	SYSTEM_CTRL_Type* _ctrl = SYSTEM_CTRL;
-	GPIO_Type *gpio = GPIO;
-	UART_Type* uart = UART;
-	CKGEN_BLE_Type* ble_clock = CKGEN_BLE;
-
-	*c = (unsigned char)(UART->DR & 0x00FF);
+	*c = (unsigned char)(uart->DR & 0x00FF);
 
 	return 0;
 }
@@ -350,12 +335,10 @@ static int uart_bluenrg_poll_in(struct device *dev, unsigned char *c)
 static void uart_bluenrg_poll_out(struct device *dev,
 					unsigned char c)
 {
-	UART_Type *uart = UART_STRUCT(dev);
-
 	/* Wait for TXE flag to be raised */
 	while (!UART_GetFlagStatus(UART_FLAG_TXFE)) {
 	}
-	uart->DR = (c & (uint16_t)0x01FF);
+	UART->DR = (c & (uint16_t)0x01FF);
 }
 
 static int uart_bluenrg_err_check(struct device *dev)
@@ -419,7 +402,6 @@ static inline void __uart_bluenrg_get_clock(struct device *dev)
 static int uart_bluenrg_fifo_fill(struct device *dev, const u8_t *tx_data,
 				  int size)
 {
-	UART_InitType *uart = UART_STRUCT(dev);
 	u8_t num_tx = 0U;
 
 	while ((size - num_tx > 0) &&
@@ -436,11 +418,9 @@ static int uart_bluenrg_fifo_fill(struct device *dev, const u8_t *tx_data,
 static int uart_bluenrg_fifo_read(struct device *dev, u8_t *rx_data,
 				  const int size)
 {
-	UART_InitType *uart = UART_STRUCT(dev);
 	u8_t num_rx = 0U;
 
-	while ((size - num_rx > 0) &&
-			UART_GetFlagStatus(UART_FLAG_RXFE)) {
+	while ((size - num_rx > 0) && UART_GetITStatus(UART_IT_RX)) {
 		/* RXNE flag will be cleared upon read from DR|RDR register */
 
 		/* Receive a character (8bit , parity none) */
@@ -449,6 +429,7 @@ static int uart_bluenrg_fifo_read(struct device *dev, u8_t *rx_data,
 		/* Clear overrun error flag */
 			UART_ClearFlag(UART_IT_OE);
 		}
+		UART_ClearITPendingBit(UART_IT_RX);
 	}
 
 	return num_rx;
@@ -456,8 +437,6 @@ static int uart_bluenrg_fifo_read(struct device *dev, u8_t *rx_data,
 
 static void uart_bluenrg_irq_tx_enable(struct device *dev)
 {
-	UART_InitType *UartInstance = UART_STRUCT(dev);
-
 	NVIC_InitType NVIC_InitStructure;
   
 	/* Enable the UART Interrupt */
@@ -475,12 +454,13 @@ static void uart_bluenrg_irq_tx_disable(struct device *dev)
 
 static int uart_bluenrg_irq_tx_ready(struct device *dev)
 {
-	return UART_GetITStatus(UART_IT_TX);
+	
+	return UART_GetITStatus(UART_IT_TXFE);
 }
 
 static int uart_bluenrg_irq_tx_complete(struct device *dev)
 {
-	return UART_GetFlagStatus(UART_FLAG_TXFE);
+	return UART_GetITStatus(UART_IT_TXFE);
 }
 
 static void uart_bluenrg_irq_rx_enable(struct device *dev)
@@ -492,7 +472,7 @@ static void uart_bluenrg_irq_rx_enable(struct device *dev)
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = LOW_PRIORITY;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
-	
+
 	UART_ITConfig(UART_IT_RX, ENABLE);
 }
 
@@ -503,8 +483,6 @@ static void uart_bluenrg_irq_rx_disable(struct device *dev)
 
 static int uart_bluenrg_irq_rx_ready(struct device *dev)
 {
-	UART_InitType *UartInstance = UART_STRUCT(dev);
-
 	return UART_GetITStatus(UART_IT_RX);
 }
 
@@ -520,16 +498,11 @@ static void uart_bluenrg_irq_err_disable(struct device *dev)
 
 static int uart_bluenrg_irq_is_pending(struct device *dev)
 {
-	UART_InitType *UartInstance = UART_STRUCT(dev);
-
 	return (
-		(UART_GetITStatus(UART_IT_RX) && UART_GetFlagStatus(UART_FLAG_RXFE)) || 
-		(UART_GetITStatus(UART_IT_TX) && UART_GetFlagStatus(UART_FLAG_TXFE))
+
+		((UART_GetITStatus(UART_IT_RX)) && UART_GetFlagStatus(UART_FLAG_RXFE)) || 
+		((UART_GetITStatus(UART_IT_TXFE)) && (UART_GetFlagStatus(UART_FLAG_TXFE)))
 	);
-	// return ((LL_USART_IsActiveFlag_RXNE(UartInstance) &&
-	// 	 LL_USART_IsEnabledIT_RXNE(UartInstance)) ||
-	// 	(LL_USART_IsActiveFlag_TC(UartInstance) &&
-	// 	 LL_USART_IsEnabledIT_TC(UartInstance)));
 }
 
 static int uart_bluenrg_irq_update(struct device *dev)
@@ -596,7 +569,11 @@ static const struct uart_driver_api uart_bluenrg_driver_api = {
 static int uart_bluenrg_init(struct device *dev)
 {
 	const struct uart_bluenrg_config *config = DEV_CFG(dev);
-	SysCtrl_PeripheralClockCmd(CLOCK_PERIPH_UART | CLOCK_PERIPH_GPIO, ENABLE);
+
+	struct device *clk = device_get_binding(BLUENRG_CLOCK_CONTROL_NAME);
+	if (clock_control_on(clk, (clock_control_subsys_t *)&config->pclken) != 0) {
+		return -EIO;
+	}
 	UART_Cmd(DISABLE);
 
 	UART_InitType UART_InitStructure;
@@ -609,8 +586,6 @@ static int uart_bluenrg_init(struct device *dev)
   	UART_InitStructure.UART_Mode = UART_Mode_Rx | UART_Mode_Tx;
   	UART_InitStructure.UART_FifoEnable = DISABLE;
   	UART_Init(&UART_InitStructure);
-
-	UART_RxFifoIrqLevelConfig(FIFO_LEV_1_64);
 	UART_Cmd(ENABLE);
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
@@ -647,8 +622,7 @@ static const struct uart_bluenrg_config uart_bluenrg_cfg_##name = {		\
 		.base = (u8_t *)DT_UART_BLUENRG_##name##_BASE_ADDRESS,\
 		BLUENRG_UART_IRQ_HANDLER_FUNC(name)			\
 	},								\
-	.pclken = { .bus = DT_UART_BLUENRG_##name##_CLOCK_BUS,	\
-		    .enr = DT_UART_BLUENRG_##name##_CLOCK_BITS	\
+	.pclken = { .bus = DT_UART_BLUENRG_##name##_CLOCK_BUS \
 	},								\
 	.hw_flow_control = DT_UART_BLUENRG_##name##_HW_FLOW_CONTROL	\
 };									\
